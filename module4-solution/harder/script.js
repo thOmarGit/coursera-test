e87c2bea891d9991d9bc41d21619f9b394eeaccf
thOmarGit <omartahtah16@gmail.com>
2021-05-09 02:07:26 +0100

    declar var i

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -1,10 +1,11 @@
-
+﻿
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
 (function(Window){
-    
+
+     var i = 0;
     for (i=0; i<names.length; i++) {
         if (names[i].charAt(0).toLowerCase()=="j") {
             byeSpeaker.speak(names[i]);
